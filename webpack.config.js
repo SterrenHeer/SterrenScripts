b79f30b8884f8e7e6bc6029c739bc5272308383d
SterrenHeer <sterrenheer@gmail.com>
2024-03-27 16:09:58 +0300

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,33 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
 	mode: 'development',
 	entry: './js/index.js',
 	output: {
 		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'js/bundle'),
-		publicPath: 'js/bundle'
-	}
+		path: path.resolve(__dirname, './js'),
+	},
+	module: {
+		rules: [
+			{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+		]
+	},
+	devServer: {
+        historyApiFallback: true,
+        static: {
+			directory: path.join(__dirname, './'),
+		  },
+        open: true,
+        compress: true,
+        hot: true,
+        port: 8080,
+    },
+	plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+    ],
 }
