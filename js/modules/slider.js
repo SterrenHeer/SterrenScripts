daa8cf54cbb84ea1ab07d5b6659eb8a120650844
SterrenHeer <sterrenheer@gmail.com>
2024-04-03 11:44:22 +0300

    refine moving of slides from events to functions

--- js/modules/slider.js
+++ js/modules/slider.js
@@ -18,36 +18,40 @@ function slider({container, slide, nextSlide, prevSlide, wrapper, field}) {
 	});
 
     next.addEventListener("click", () => {
-		if (offset == deleteNotDigits(width) * (slides.length - 1)) {
+		moveNext();
+	});
+
+    prev.addEventListener("click", () => {
+		movePrev();
+	});
+
+	function moveNext() {
+        if (offset == deleteNotDigits(width) * (slides.length - 1)) {
 			offset = 0;
 		} else {
 			offset += deleteNotDigits(width);
 		}
-
-		slidesField.style.transform = `translateX(-${offset}px)`;
 
 		if (slideIndex == slides.length) {
 			slideIndex = 1;
 		} else {
 			slideIndex++;
 		}
-	});
+    }
 
-    prev.addEventListener("click", () => {
-		if (offset == 0) {
+    function movePrev() {
+        if (offset == 0) {
 			offset = deleteNotDigits(width) * (slides.length - 1);
 		} else {
 			offset -= deleteNotDigits(width);
 		}
-
-		slidesField.style.transform = `translateX(-${offset}px)`;
 
 		if (slideIndex == 1) {
 			slideIndex = slides.length;
 		} else {
 			slideIndex--;
 		}
-	});
+    }
 
     function deleteNotDigits(str) {
         return +str.replace(/[^\d\.]/g, '');
